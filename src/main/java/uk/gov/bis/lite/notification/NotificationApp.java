080aeb2110bf0d58e697f34cd5b5aa423b1e12fd
Dan Haynes <djm.haynes@gmail.com>
2017-01-05 17:40:50 +0000

    LITE-287 Dropwizard upgraded to 1.0.5, correlation id update, config update

--- src/main/java/uk/gov/bis/lite/notification/NotificationApp.java
+++ src/main/java/uk/gov/bis/lite/notification/NotificationApp.java
@@ -8,6 +8,7 @@ import org.flywaydb.core.Flyway;
 import ru.vyarus.dropwizard.guice.GuiceBundle;
 import ru.vyarus.dropwizard.guice.module.installer.feature.ManagedInstaller;
 import ru.vyarus.dropwizard.guice.module.installer.feature.jersey.ResourceInstaller;
+import uk.gov.bis.lite.common.jersey.filter.ContainerCorrelationIdFilter;
 import uk.gov.bis.lite.notification.config.GuiceModule;
 import uk.gov.bis.lite.notification.config.NotificationAppConfig;
 import uk.gov.bis.lite.notification.exception.NotificationServiceException;
@@ -32,6 +33,7 @@ public class NotificationApp extends Application<NotificationAppConfig> {
   public void run(NotificationAppConfig configuration, Environment environment) throws Exception {
 
     environment.jersey().register(NotificationServiceException.ServiceExceptionMapper.class);
+    environment.jersey().register(ContainerCorrelationIdFilter.class);
 
     // Perform/validate flyway migration on startup
     DataSourceFactory dataSourceFactory = configuration.getDataSourceFactory();
