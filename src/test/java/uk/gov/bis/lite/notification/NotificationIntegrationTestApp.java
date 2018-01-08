477b30110cd2d4046c4962b05e650e06e5e38567
Laurence Cartwright <laurence.cartwright@fivium.co.uk>
2018-01-08 11:10:06 +0000

    Run flyway migration as normal with NotificationIntegrationTestApp

--- src/test/java/uk/gov/bis/lite/notification/NotificationIntegrationTestApp.java
+++ src/test/java/uk/gov/bis/lite/notification/NotificationIntegrationTestApp.java
@@ -1,15 +1,8 @@
 package uk.gov.bis.lite.notification;
 
-import uk.gov.bis.lite.notification.config.NotificationAppConfig;
-
 public class NotificationIntegrationTestApp extends NotificationApp {
 
   public NotificationIntegrationTestApp() {
     super(new NotificationIntegrationTestModule());
-  }
-
-  @Override
-  protected void flywayMigrate(NotificationAppConfig configuration) {
-
   }
 }
